b5d408355a9bc4ca75f056a9cc6e6eb409e216dd
Andrew Armstrong <andrew.armstrong@iress.com>
2022-01-13 20:18:23 +1100

    Implemented robot::move().

--- src/robot.cpp
+++ src/robot.cpp
@@ -32,6 +32,34 @@ bool Robot::place(std::shared_ptr<ISurface> surface, int x, int y, Direction fac
 
 bool Robot::move()
 {
+    if (!m_surface)
+        return false;
+
+    int x = m_x;
+    int y = m_y;
+
+    switch(m_facing)
+    {
+    case North:
+        y++;
+        break;
+    case East:
+        x++;
+        break;
+    case South:
+        y--;
+        break;
+    case West:
+        x--;
+        break;
+    }
+
+    if (!m_surface->is_valid_location(x, y))
+        return false;
+
+    m_x = x;
+    m_y = y;
+
     return true;
 }
 
